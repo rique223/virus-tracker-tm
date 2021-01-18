65b23cf1834a5f6b09991499365eaed4c0501c26
Calebe Xavier <calebexavier.developer@gmail.com>
2021-01-18 18:27:25 -0300

    TextBox criado

--- src/Components/TextBox/TextBox.js
+++ src/Components/TextBox/TextBox.js
@@ -1,12 +1,27 @@
 import './TextBox.css';
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Text, Flex} from "@chakra-ui/react";
 
 function TextBox() {
   return (
-      <Box padding="4" maxW="3xl">
-          There are many benefits to a joint design and development system. Not only
-          does it bring benefits to the design team.
-      </Box>
+      <Flex>
+        <Box padding="4" maxW="3xl" alignItems="center">
+            <Text 
+                bgClip="text"
+                fontSize="6xl"
+                color="#20481E"             
+                fontWeight="bold"
+                width="60%" 
+                justifyContent="center"
+                flexWrap="wrap"
+                margin="20%"
+                textAlign="center">
+                O TheVirusTracker™ pretende trazer praticidade para a visualização de 
+                dados relacionados à pandemia de Corona Vírus no Brasil: 
+                Digite o nome de uma cidade brasileira acima e serão retornadas as principais 
+                estatísticas relacionadas ao avanço do Sars-CoV-2 neste local.
+                </Text>
+        </Box>
+      </Flex>
   );
 }
 
