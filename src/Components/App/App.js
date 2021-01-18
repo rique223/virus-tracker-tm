423c6ace9effb5cb155beb3fc8d6bbe81a4871af
Henrique Guimarães Ribeiro <henrique.jobs1@gmail.com>
2021-01-18 17:26:54 -0300

    Criado componente background

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import logo from '../../logo.svg';
+import './App.css';
+import { Box, Container } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Container maxW="xl" h="100%">
+      <Box bg="#EAEFEC" w="100%" h="100%" p={0} color="#20481E">
+        <Box bg="#5CDB96" w="100%" h="33%" p={0} color="White">
+          
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
