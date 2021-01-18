04c12db7743df4d502ddb62c215aa168aba047ef
Henrique Guimarães Ribeiro <henrique.jobs1@gmail.com>
2021-01-18 17:29:30 -0300

    Componente Background reestruturado
    Devido à um erro de design tive de reestruturar o código do componente Background.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,10 +5,11 @@ import { Box, Container } from "@chakra-ui/react";
 function App() {
   return (
     <Container maxW="xl" h="100%">
-      <Box bg="#EAEFEC" w="100%" h="100%" p={0} color="#20481E">
-        <Box bg="#5CDB96" w="100%" h="33%" p={0} color="White">
+      <Box bg="#5CDB96" w="100%" h="33%" p={0} color="White">
           
-        </Box>
+      </Box>
+      <Box bg="#EAEFEC" w="100%" h="66%" p={0} color="#20481E">
+
       </Box>
     </Container>
   );
