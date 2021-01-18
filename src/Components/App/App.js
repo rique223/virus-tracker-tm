9765ef18c00bed7d16610662963b30edd7081d1a
Henrique Guimarães Ribeiro <henrique.jobs1@gmail.com>
2021-01-18 18:26:11 -0300

    Consertados erros de css

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-import { Box, Container, Center } from "@chakra-ui/react";
+import { Box, Container, Center, Flex, Spacer } from "@chakra-ui/react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVirus } from '@fortawesome/free-solid-svg-icons';
 import TextBox from '../TextBox/TextBox';
 
 function App() {
   return (
-    <Container maxW="xl" h="100%">
-      <Box bg="#5CDB96" w="100%" h="33%" p={0} color="White">
-          
+    <Container maxW="100%" h="100%">
+      <Box bg="#5CDB96" w="100%" h="33%" p={4} color="White">
+        <Flex>
+          <Box p="4">
+            <span><FontAwesomeIcon icon={faVirus} size="lg"/>TheVirusTracker™</span>
+          </Box>
+          <Spacer />
+          <Box p="4" >
+            Box 2
+          </Box>
+        </Flex>
       </Box>
       <Box bg="#EAEFEC" w="100%" h="66%" p={0} color="#20481E" alignItems="center">
         <Center color="#20481E" h="100%">
