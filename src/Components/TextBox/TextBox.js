0b253edc04c2d5b737a9e09da6f2ec1ab6e7fff5
Henrique Guimarães Ribeiro <henrique.jobs1@gmail.com>
2021-01-18 18:36:11 -0300

    Corrigidos erros no texto de CTA

--- src/Components/TextBox/TextBox.js
+++ src/Components/TextBox/TextBox.js
@@ -1,27 +1,22 @@
 import './TextBox.css';
-import { Box, Text, Flex} from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 function TextBox() {
   return (
-      <Flex>
-        <Box padding="4" maxW="3xl" alignItems="center">
-            <Text 
-                bgClip="text"
-                fontSize="6xl"
-                color="#20481E"             
-                fontWeight="bold"
-                width="60%" 
-                justifyContent="center"
-                flexWrap="wrap"
-                margin="20%"
-                textAlign="center">
-                O TheVirusTracker™ pretende trazer praticidade para a visualização de 
-                dados relacionados à pandemia de Corona Vírus no Brasil: 
-                Digite o nome de uma cidade brasileira acima e serão retornadas as principais 
-                estatísticas relacionadas ao avanço do Sars-CoV-2 neste local.
-                </Text>
-        </Box>
-      </Flex>
+    <Box padding="4" maxW="3xl">
+        <Text 
+            bgClip="text"
+            fontSize="38px"
+            color="#20481E" 
+            fontWeight="bold"
+            textAlign="center"
+            px="10%">
+            O TheVirusTracker™ pretende trazer praticidade para a visualização de 
+            dados relacionados à pandemia de Corona Vírus no Brasil: 
+            Digite o nome de uma cidade brasileira acima e serão retornadas as principais 
+            estatísticas relacionadas ao avanço do Sars-CoV-2 neste local.
+        </Text>
+    </Box>
   );
 }
 
