dc652477b1e74531e6bc34d145e5ffa8e091eef3
Henrique Guimarães Ribeiro <henrique.jobs1@gmail.com>
2021-01-27 17:16:08 -0300

    Retificação do merge

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -8,15 +8,7 @@ import { extendTheme } from "@chakra-ui/react";
 import React, { FunctionComponent, ReactElement, useState } from "react";
 import Searchbar from "../searchBar/Searchbar";
 import apiCNPJ from "../../api/covidAPI";
-import TextCity from '../TextCity/TextCity';
-
-const theme: FunctionComponent = extendTheme({
-  fonts: {
-    body: "Roboto, sans-serif",
-    heading: "Roboto, sans-serif",
-    mono: "Roboto, sans-serif",
-  },
-})
+import TextCity from "../TextCity/TextCity";
 
 const App: React.FC = (): ReactElement => {
   const [covidData, setCovidData] = useState({} as Models.CovidData);
@@ -49,19 +41,27 @@ const App: React.FC = (): ReactElement => {
 
   return (
     <ChakraProvider theme={theme}>
-        <Container maxW="100%" m={0} p={0} maxH="100%">
-          <Box bg="#5CDB96" w="100%" maxH="40%" h="40%" p={2} color="White">
-            <NavBar></NavBar>
-            <Searchbar searchCOVID={searchCOVID} />
-            <TextCity />
-          </Box>
-          
-          <Box bg="#EAEFEC" w="100%" maxH="60%" h="60%" p={0} color="#20481E" alignItems="center">
-            <Center color="#20481E" h="100%">
-              <TextBox />
-            </Center>
-          </Box>
-        </Container>
+      <Container maxW="100%" m={0} p={0} maxH="100%">
+        <Box bg="#5CDB96" w="100%" maxH="40%" h="40%" p={2} color="White">
+          <NavBar></NavBar>
+          <Searchbar searchCOVID={searchCOVID} />
+          <TextCity />
+        </Box>
+
+        <Box
+          bg="#EAEFEC"
+          w="100%"
+          maxH="60%"
+          h="60%"
+          p={0}
+          color="#20481E"
+          alignItems="center"
+        >
+          <Center color="#20481E" h="100%">
+            <TextBox />
+          </Center>
+        </Box>
+      </Container>
     </ChakraProvider>
   );
 };
