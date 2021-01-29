b576de4d92cfe2e8c89db79124ed2bb93cfdb611
Henrique Guimarães Ribeiro <henrique.jobs1@gmail.com>
2021-01-29 18:29:16 -0300

    Pequenas modificações no estilo dos componentes

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -5,30 +5,34 @@ import { ListItem, Center, Text, Link } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 const ItemList = (props: {
-  name: string;
-  icon: ReactElement;
-  route: string;
+	name: string;
+	icon: ReactElement;
+	route: string;
 }) => {
-  return (
-    <ListItem listStyleType="none" _hover={{ backgroundColor: "#cccccc" }}>
-      <Center w="100%" h="4rem">
-        <Link
-          as={ReactRouterLink}
-          to={props.route}
-          _hover={{
-            textDecoration: "none",
-          }}
-          _focus={{ boxShadow: "none" }}
-        >
-          <Text textTransform="uppercase" color="#20481E" fontSize="20px">
-            {props.icon}
-            &nbsp;
-            {props.name}
-          </Text>
-        </Link>
-      </Center>
-    </ListItem>
-  );
+	return (
+		<ListItem listStyleType="none" _hover={{ backgroundColor: "#cccccc" }}>
+			<Center w="100%" h="4rem">
+				<Link
+					as={ReactRouterLink}
+					to={props.route}
+					_hover={{
+						textDecoration: "none",
+					}}
+					w="100%"
+					h="100%"
+					display="flex"
+					alignItems="center"
+					justifyContent="center"
+				>
+					<Text textTransform="uppercase" color="#20481E" fontSize="20px">
+						{props.icon}
+						&nbsp;
+						{props.name}
+					</Text>
+				</Link>
+			</Center>
+		</ListItem>
+	);
 };
 
 export default ItemList;
